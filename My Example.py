165e7d7f7e94360f8d695549f1258d0ffbf84a61
asvarnon <austinvarnon@gmail.com>
2021-02-03 15:45:20 -0600

    Dictionaries

--- My Example.py
+++ My Example.py
@@ -22,16 +22,20 @@ taxes = myIncome*taxRate
 # print("This result was: {r:1.6f}".format(r=result))
 
 
-newList = [1, 2, 3, 4, 5]
-print(newList)
-newList.append(6)
-print(newList)
-newList.pop()
-print(newList)
+# newList = [1, 2, 3, 4, 5]
+# print(newList)
+# newList.append(6)
+# print(newList)
+# newList.pop()
+# print(newList)
 
-poppedItem = newList.pop(3)
-print(poppedItem)
+# poppedItem = newList.pop(3)
+# print(poppedItem)
 
+myDict = {'key1':'val1', 'key2':'val2'}
+print(myDict['key2'])
 
+priceLookUp = {'apple':2.99, 'oranges':3.99, 'milk':1.99}
+print(priceLookUp['oranges'])
 
 
