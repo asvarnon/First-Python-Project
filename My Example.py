25d922786b3273f110e97f82dc9654014228be32
asvarnon <austinvarnon@gmail.com>
2021-02-03 15:26:35 -0600

    Lists Exercises and examples

--- My Example.py
+++ My Example.py
@@ -17,6 +17,20 @@ taxes = myIncome*taxRate
 #     randomString[7:]   
 # )
 
+# result = 100/777
+# print("This result was:{r}".format(r=result))
+# print("This result was: {r:1.6f}".format(r=result))
+
+
+newList = [1, 2, 3, 4, 5]
+print(newList)
+newList.append(6)
+print(newList)
+newList.pop()
+print(newList)
+
+poppedItem = newList.pop(3)
+print(poppedItem)
 
 
 
