ddfae75b491ccff36b8e3dcb0acf0b9d9f24c661
asvarnon <austinvarnon@gmail.com>
2021-02-03 14:29:15 -0600

    Strings exercise ext

--- My Example.py
+++ My Example.py
@@ -2,9 +2,17 @@ myIncome = 100
 taxRate = 0.1
 taxes = myIncome*taxRate
 
-type(taxes)
+print(type(taxes))
 print(taxes)
 
 randomString = "Hi how are you"
 print(len(randomString))
 
+
+print(
+    randomString[0],
+    randomString[0:3]
+)
+print(
+    randomString[7:]   
+)
