aaa9d0238e9c599eeecd5efab10a42895d54c14f
asvarnon <austinvarnon@gmail.com>
2021-02-08 14:37:59 -0600

    for loops complete

--- loopsAndConditionals.py
+++ loopsAndConditionals.py
@@ -6,11 +6,56 @@
 #     print('ITS FALSE')
 
 
-name = "Austin"
+#Conditionals
+# name = "Austin"
+# if name == "Frankie":
+#     print(f"Hello {name}!")
+# elif name == "Austin":
+#     print(f"Hello {name}!")
+# else:
+#     print("What is your name?")
 
-if name == "Frankie":
-    print(f"Hello {name}!")
-elif name == "Austin":
-    print(f"Hello {name}!")
-else:
-    print("What is your name?")
+#For loops
+# myNames = ['Austin','Frankie','Clayton']
+# for name in myNames:
+#     if name == "Frankie":
+#         print(f"Hello {name}!")
+#     elif name == "Austin":
+#         print(f"Hello {name}!")
+#     else:
+#         print("What is your name?")
+
+# myNumbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+# for num in myNumbers:
+#     if num % 2 ==0:
+#         print(num)
+
+# listSum = 0
+# for num in myNumbers:
+#     listSum = listSum + num
+# print(listSum)
+
+# myString = "Hello World"
+# for letter in myString:
+#     print(letter)
+
+
+#--------------TUPLES-------------------
+# myList = [(1,2),(3,4),(5,6),(7,8)]
+# print(len(myList))
+#Tuple unpacking-----
+# for a,b in myList:
+#     print(a)
+#     print(b)
+
+# myList = [(1,2,3),(4,5,6),(7,8,9)]
+# for a,b,c in myList:
+#     print(b)
+
+#---------------Dictionaries----------------
+d = {'key1':1, 'key2':2, 'key3':3}
+#default iterates through keys
+for key,value in d.items():
+    print(value)
+    print(key)
+
