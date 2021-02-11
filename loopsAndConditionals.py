d837c224a9ab8ca1fc3cd19eacd782374f3fc503
asvarnon <austinvarnon@gmail.com>
2021-02-10 19:34:50 -0600

    slight change

--- loopsAndConditionals.py
+++ loopsAndConditionals.py
@@ -205,20 +205,21 @@
 #         print(word)
 
 ##5)
-#Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
-# for num in range(1,101):
-#     if num%3 == 0:
-#         print(num, 'Fizz')
-#     if num%5 == 0:
-#         print(num, 'Buzz')
-#     if num%3 == 0 and num%5 == 0:
-#         print(num, 'FizzBuzz')
+# Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+for num in range(1,101):
+    if num%3 == 0:
+        print(num, 'Fizz')
+    if num%5 == 0:
+        print(num, 'Buzz')
+    if num%3 == 0 and num%5 == 0:
+        print(num, 'FizzBuzz')
 
 
 ##6)
 st = 'Create a list of the first letters of every word in this string'
 myList =[word[0] for word in st.split()]
 print(myList)
+
 
 
 
