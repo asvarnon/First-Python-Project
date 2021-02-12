17d7928aeaa4e6eff65cc2df50a011f1cb18a21e
asvarnon <austinvarnon@gmail.com>
2021-02-12 13:52:44 -0600

    basic method structuring

---
+++ methodsAndFunctions.py
@@ -0,0 +1,13 @@
+def helloPerson(name):
+    print(f"Hello {name}")
+
+def addNumbers(a, b):
+    return a + b
+
+def squareAddedNum(num):
+    return num**2
+
+
+print(addNumbers(2, 4))
+print(squareAddedNum(addNumbers(2,4)))
+
