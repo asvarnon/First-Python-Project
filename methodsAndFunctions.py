212b47ae88f577f13c7054a5a0f1d5f29e7c2fa8
asvarnon <austinvarnon@gmail.com>
2021-02-12 15:06:58 -0600

    Made file creation method, typed content, and closed file

--- methodsAndFunctions.py
+++ methodsAndFunctions.py
@@ -1,13 +1,21 @@
-def helloPerson(name):
-    print(f"Hello {name}")
+# def helloPerson(name):
+#     print(f"Hello {name}")
 
-def addNumbers(a, b):
-    return a + b
+# def addNumbers(a, b):
+#     return a + b
 
-def squareAddedNum(num):
-    return num**2
+# def squareAddedNum(num):
+#     return num**2
 
 
-print(addNumbers(2, 4))
-print(squareAddedNum(addNumbers(2,4)))
+# addedNumber = addNumbers(2, 4)
+# print(squareAddedNum(addedNumber))
 
+def createFile():
+    # windowsFile = open("Desktop", "w")
+    macFile = open("/Users/austinvarnon/Desktop/testing.txt", "w")
+    macFile.write("Hi \n")
+    macFile.writelines("lololol...")
+    macFile.close()
+    
+createFile()
