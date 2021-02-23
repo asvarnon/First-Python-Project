3a00b901fffeed866b38bbabb219b78bd6cb9a1c
asvarnon <austinvarnon@gmail.com>
2021-02-23 11:51:53 -0600

    LEvel two solutions

--- methodsAndFunctions.py
+++ methodsAndFunctions.py
@@ -167,11 +167,11 @@ def paperDoll(str):
 def blackjack(a, b, c):
     if sum([a, b, c]) <= 21:
         return sum([a, b, c])
-    elif 11 in [a,b,c] and sum([a,b,c]) - 10 <= 21:
-        return sum([a,b,c])
+    elif 11 in [a,b,c] and sum([a,b,c]) <= 31:
+        return sum([a, b, c]) - 10 
     else:
         return "BUST"
 
-print(blackjack(2,10,11))
+print(blackjack(2,10,10))
 
 
